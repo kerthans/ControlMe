b1a281eeb6f332fc1ac7af0be743215d5ddd632e
kerthans <1255829396@qq.com>
2024-11-23 13:20:14 +0800

    初步上线

---
+++ controlme/controlmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  controlmeApp.swift
+//  controlme
+//
+//  Created by clint on 2024/11/23.
+//
+
+import SwiftUI
+
+@main
+struct controlmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ controlmeTests/controlmeTests.swift
@@ -0,0 +1,16 @@
+//
+//  controlmeTests.swift
+//  controlmeTests
+//
+//  Created by clint on 2024/11/23.
+//
+
+import Testing
+
+struct controlmeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
